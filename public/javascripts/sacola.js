4403aef2ad1f2e31d54dc5c376d97d09dfc94873
M@schie1 <moschiel@hotmail.com>
2020-11-12 21:29:07 -0300

    usando sessionStorage para armazenar items da sacola

--- public/javascripts/sacola.js
+++ public/javascripts/sacola.js
@@ -1,90 +1,133 @@
-//inverte visualizacao da sacola
-const toggleSacola = () => {
-  if(document.body.querySelector('.sacola').style.display != 'flex'){
-    document.body.querySelector('.sacola').style.display = 'flex';
-  }else{
-    document.body.querySelector('.sacola').style.display = 'none';
+let itemsSacola = [];
+let sacolaSessionKey = "sacolaSessionStorage";
+
+// inicializa sacola com items armazenados na sessionStorage
+function inicializaSacola() {
+  let sessionItemsSacola = sessionStorage.getItem(sacolaSessionKey);
+  if(sessionItemsSacola != null) {
+    itemsSacola = JSON.parse(sessionItemsSacola);
+
+    let itemsHtml = "";
+    itemsSacola.forEach(item => {
+      itemsHtml += criarHtmlDoItemParaSacola(item);
+    });
+
+    let elSacolaLista = document.getElementById('sacola-lista');
+    elSacolaLista.insertAdjacentHTML("beforeend", itemsHtml); //adiciona items no HTML
+    
+    atualizarTotalSacola();
   }
 }
 
-const lerProdutoDoCard = (card_id) => {
-    //seleciona o card
-    let cardElement = document.getElementById(card_id);
-    //faz a leitura dos dados do Card
-    let produto = {
-      nome: cardElement.querySelector(".my-card-title").innerHTML,
-      img: cardElement.querySelector(".my-card-img-top").src,
-      valor: cardElement.querySelector(".my-card-text").innerHTML
-    }
-    return produto; 
+//numero para moeda real
+function numberToReal(num) {
+  return num.toFixed(2).replace('.', ',');
+}
+//moeda real para numero
+function realToNumber (real) {
+  return Number(real.toString().replace(',', '.'));
 }
 
-const criarHtmlDoProdutoParaSacola = (element_id, produto) => {
-  let prodHtml = `
-  <li class="sacola-item" id="${element_id}">
+
+//inverte visualizacao da sacola
+function toggleSacola() {
+  let elSacola = document.body.querySelector('.sacola');
+  if(elSacola.style.display != 'flex'){
+    elSacola.style.display = 'flex';
+  }else{
+    elSacola.style.display = 'none';
+  }
+}
+
+
+function criarItemSacolaComValoresDoCard(card_id) {
+    //seleciona o card
+    let elCard = document.getElementById(card_id);
+    //faz a leitura dos dados do Card
+    let item = {
+      id: "sacola-item-" + card_id,
+      nome: elCard.querySelector(".my-card-title").innerHTML,
+      valor: realToNumber(elCard.querySelector(".my-card-text").innerHTML),
+      img: elCard.querySelector(".my-card-img-top").src,
+      quantidade: 1
+    }
+    return item; 
+}
+
+function criarHtmlDoItemParaSacola(item) {
+  let itemHtml = `
+  <li class="sacola-item" id="${item.id}">
     <div class="item-seletor">
-      <i class="fas fa-minus" onclick="mudarQuantidadeItemSacola('${element_id}', '${produto.valor}', '-')"></i>
-      <span class="item-quantidade">1</span>
-      <i class="fas fa-plus" onclick="mudarQuantidadeItemSacola('${element_id}', '${produto.valor}', '+')"></i>
+      <i class="fas fa-minus" onclick="mudarQuantidadeItemSacola('${item.id}', '-')"></i>
+      <span class="item-quantidade">${item.quantidade}</span>
+      <i class="fas fa-plus" onclick="mudarQuantidadeItemSacola('${item.id}', '+')"></i>
     </div>
-    <img src="${produto.img}" alt="">
-    <span class="item-nome">${produto.nome}</span>
-    <span class="item-preco">R$ <span class="item-valor">${produto.valor}</span></span>
-    <i class="fas fa-trash-alt" onclick="removerItemDaSacola('${element_id}')"></i>
+    <img src="${item.img}" alt="">
+    <span class="item-nome">${item.nome}</span>
+    <span class="item-preco">R$ <span class="item-valor">${numberToReal(item.valor)}</span></span>
+    <i class="fas fa-trash-alt" onclick="removerItemDaSacola('${item.id}')"></i>
   </li>`;
 
-  return prodHtml;
+  return itemHtml;
 }
 
-//atualiza quantidade de elementos no icone da sacola
-//atualiza o preco total na sacola
-const calcularTotalSacola = () => {
-  let iconeSacola = document.querySelector('.icone-sacola-num');
-  let sacola = document.querySelector('.sacola');
-  let valoresElement = sacola.querySelectorAll('.item-valor');
-  let quantElement = sacola.querySelectorAll('.item-quantidade');
 
+//atualiza o preco total na sacola
+//atualiza quantidade total de elementos no icone da sacola
+//atualiza sessionStorage da sacola
+function atualizarTotalSacola() {
+  let elIconeSacola = document.querySelector('.icone-sacola-num');
+  let elSacola = document.body.querySelector('.sacola');
+  let elValores = elSacola.querySelectorAll('.item-valor');
+  let elPreco = elSacola.querySelector('.sacola-total-valor');
+  let elQuant = elSacola.querySelectorAll('.item-quantidade');
   
   let totalPreco = 0.0;
   let totalQuant = 0;
-  for(let i=0; i < valoresElement.length; i++){
-    //no preço, substitui virgula por ponto para fazer calculos
-    let val = valoresElement[i].innerHTML.replace(',','.');
-    totalPreco += Number(val);
-    let quant = quantElement[i].innerHTML;
-    totalQuant += Number(quant);
+  for(let i=0; i < elValores.length; i++){
+    //soma preço
+    totalPreco += realToNumber(elValores[i].innerHTML);
+    //soma quantidade
+    totalQuant += Number(elQuant[i].innerHTML);
   }
-  totalPreco = totalPreco.toFixed(2).replace('.',','); //coloca virgula de volta
-  sacola.querySelector('.sacola-total-valor').innerHTML = totalPreco; //atualiza preço total
-
-  iconeSacola.innerHTML = totalQuant; //atualizar quantidade total no icone
+  //atualiza preço total
+  elPreco.innerHTML = numberToReal(totalPreco); 
+  //aqualiza quantidade total
+  if(elIconeSacola != null) {
+    elIconeSacola.innerHTML = totalQuant;
+  }
+  //atualiza sessionStorage da sacola
+  // console.log(itemsSacola);
+  sessionStorage.setItem(sacolaSessionKey, JSON.stringify(itemsSacola));
 }
 
-const adicionarNaSacola = (card_id)=>{
-  let produto = lerProdutoDoCard(card_id);
-  let sacolaItemId = 'sacola-item-' + card_id;
-  let sacolaLista = document.getElementById('sacola-lista');
+function adicionarNaSacola(card_id) {
+  let item = criarItemSacolaComValoresDoCard(card_id);
+  let elSacolaLista = document.getElementById('sacola-lista');
 
   //evita adicionar itens repetidos
-  if(sacolaLista.querySelector('#' + sacolaItemId) == null){
-    let produtoHtml = criarHtmlDoProdutoParaSacola(sacolaItemId, produto);  
-    sacolaLista.insertAdjacentHTML("beforeend", produtoHtml);
-    calcularTotalSacola(); //atualiza total
+  if(elSacolaLista.querySelector('#' + item.id) == null){
+    let itemHtml = criarHtmlDoItemParaSacola(item);
+    elSacolaLista.insertAdjacentHTML("beforeend", itemHtml); //adiciona no HTML
+    itemsSacola.push(item); //adiciona no array
+    atualizarTotalSacola(); //atualiza total
   }
 }
 
-const removerItemDaSacola = (sacola_item_id)=> {
-  document.getElementById(sacola_item_id).remove();
-  calcularTotalSacola(); //atualiza total
+function removerItemDaSacola(sacola_item_id) {
+  document.getElementById(sacola_item_id).remove(); //remove do HTML
+  itemsSacola = itemsSacola.filter(item => item.id != sacola_item_id); //remove do Array
+  atualizarTotalSacola(); //atualiza total
 }
 
 
-const mudarQuantidadeItemSacola = (sacola_item_id, item_valor, operacao) => {
-  let itemElement = document.getElementById(sacola_item_id);
+function mudarQuantidadeItemSacola(sacola_item_id, operacao) {
+  let elItem = document.getElementById(sacola_item_id); //referencia HTML
+  let itemIndex = itemsSacola.findIndex(item => item.id == sacola_item_id); //referencia array
 
-  //********* atualiza quantidade ************
-  let quantElement = itemElement.querySelector('.item-quantidade');
-  let quantidade = Number(quantElement.innerHTML);
+  //********* atualizando quantidade do item ************
+  let elQuant = elItem.querySelector('.item-quantidade');
+  let quantidade = itemsSacola[itemIndex].quantidade;
   
   if(operacao == '-'){
     if(quantidade > 1) {
@@ -93,14 +136,16 @@ const mudarQuantidadeItemSacola = (sacola_item_id, item_valor, operacao) => {
   }else { //operacao == '+'
     quantidade ++;
   }
-  quantElement.innerHTML = quantidade; 
-  
-  //************* atualiza preço do item *****************
-  item_valor = item_valor.replace(',','.'); //para fazer contas, usa-se ponto ao invés de virgula
-  let valorElement = itemElement.querySelector('.item-valor');
-  item_valor = (item_valor * quantidade).toFixed(2);
-  item_valor = item_valor.replace('.',','); //coloca a virgula de volta
-  valorElement.innerHTML = item_valor; 
+  elQuant.innerHTML = quantidade; //atualiza no HTML
+  itemsSacola[itemIndex].quantidade = quantidade; //atualiza no Array
 
-  calcularTotalSacola(); //atualiza total
+  
+  //************* atualiza preço do item no HTML *****************
+  let elValor = elItem.querySelector('.item-valor');
+  let precoUnitario = itemsSacola[itemIndex].valor;
+  elValor.innerHTML = numberToReal(precoUnitario * quantidade); 
+
+  atualizarTotalSacola(); //atualiza total
 }
+
+inicializaSacola();
