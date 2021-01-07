c85451ed848bc39086cd7fc8940313c70435c214
M@schie1 <moschiel@hotmail.com>
2021-01-07 18:32:36 -0300

    adicionado db-migration para os cartoes de pagamento

---
+++ models/Cards.js
@@ -0,0 +1,32 @@
+
+module.exports = (sequelize, DataType) => {
+	const Cards = sequelize.define(
+		'Cards', 
+	{
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    selected: DataType.INTEGER,
+    type: DataType.STRING,
+    number: DataType.STRING,
+    expirationDate: DataType.STRING,
+    cvv: DataType.STRING,
+    holderName: DataType.STRING,
+    holderCpf: DataType.STRING,
+    deleted: {
+      type: DataType.INTEGER,
+      defaultValue: 0
+    },
+    createdAt: {
+      type: DataType.DATE
+    },
+    updatedAt: {
+      type: DataType.DATE
+    }
+	},{
+    tableName: 'cards',
+	});
+	return Cards;
+};
