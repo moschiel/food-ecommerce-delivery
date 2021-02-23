0770e33985be902c96f4d85555188fbe6c9a44db
Marco Campos <marcoaadc@gmail.com>
2021-02-23 20:26:12 -0300

    criando função list, create e delete do produto e função list do pedido

--- controllers/ShopkeeperController.js
+++ controllers/ShopkeeperController.js
@@ -1,128 +1,57 @@
-let categorias = [
-    {
-      nome: "Lanches",
-      codigo: 1,
-      items: [
-        {
-          id: 1,
-          nome: "x-python",
-          valor: "15,99",
-          imagem: "./images/food1.jpg"
-        },
-        {
-          id: 2,
-          nome: "x-C++",
-          valor: "14,99",
-          imagem: "./images/food2.jpg"
-        },
-        {
-          id: 3,
-          nome: "x-C#",
-          valor: "14,89",
-          imagem: "./images/food3.jpg"
-        },
-        {
-          id: 4,
-          nome: "x-Javascript",
-          valor: "13,89",
-          imagem: "./images/food3.jpg"
-        },
-        {
-          id: 5,
-          nome: "x-Go Lang",
-          valor: "10,59",
-          imagem: "./images/food1.jpg"
-        },
-        {
-          id: 6,
-          nome: "x-dart",
-          valor: "16,59",
-          imagem: "./images/food2.jpg"
-        }
-      ]
-    },
-    {
-      nome: "Pizzas",
-      codigo: 2,
-      items: [
-        {
-          id: 21,
-          nome: "Moda do Java",
-          valor: "45,99",
-          imagem: "./images/food1.jpg"
-        },
-        {
-          id: 22,
-          nome: "Pyzza",
-          valor: "44,99",
-          imagem: "./images/food2.jpg"
-        },
-        {
-          id: 23,
-          nome: "C# à italiana",
-          valor: "44,89",
-          imagem: "./images/food3.jpg"
-        },
-        {
-          id: 24,
-          nome: "Dartguesa",
-          valor: "43,89",
-          imagem: "./images/food3.jpg"
-        },
-        {
-          id: 25,
-          nome: "Go Lang ",
-          valor: "40,59",
-          imagem: "./images/food1.jpg"
-        },
-      ]
-    },
-    {
-      nome: "Bebidas",
-      codigo: 3,
-      items: [
-        {
-          id: 31,
-          nome: "Coca-Cola",
-          valor: "108,99",
-          imagem: "./images/food1.jpg"
-        },
-        {
-          id: 32,
-          nome: "Fanta",
-          valor: "8,99",
-          imagem: "./images/food2.jpg"
-        },
-        {
-          id: 33,
-          nome: "Sprite",
-          valor: "8,89",
-          imagem: "./images/food3.jpg"
-        },
-        {
-          id: 34,
-          nome: "Sukita",
-          valor: "8,89",
-          imagem: "./images/food3.jpg"
-        },
-        {
-          id: 35,
-          nome: "Guaraná Antártica",
-          valor: "7,59",
-          imagem: "./images/food1.jpg"
-        },
-        {
-          id: 36,
-          nome: "Funada",
-          valor: "5,59",
-          imagem: "./images/food2.jpg"
-        }
-      ]
-    },
-  ]
+const {Product, Sequelize} = require('../models');
   
   module.exports = {
-    lojista: (req,res)=>{
-      res.render('lojista', { title: 'Ecommerce', categorias});
-    }
-  } 
+    // funcao de listagem de produtos| com filtro de produtos não excluidos
+   async list (req,res,next) {
+      
+      let products = await Product.findAll({
+        where:{
+        deleted: 0
+      }
+    });
+     
+      res.render('lojista', {products});
+    },
+
+    // funcao de criacao de produtos *corrigir redirecionamento, esta dando erro na pagina apos cadastar produto
+    async create(req, res, next) {      
+      let product = { ...req.body }; 
+      await Product.create(product);
+
+      let products = await Product.findAll({
+        where:{
+        deleted: 0
+      }
+    });
+   
+  
+      res.render('lojista', {products});
+    },
+
+    // falta funcao de alterar e atualizar produtos
+
+    // funcao de deletar produtos *corrigir redirecionamento, erro na pagina apos excluir o produto
+    async delete(req, res, next) {
+      let id = req.params.id;
+      let product = await Product.findByPk(id);
+  
+      product.deleted = 1;
+  
+      await product.save();    
+  
+      let products = await Product.findAll({
+        where:{
+        deleted: 0
+      }
+      });
+      
+      res.redirect('/lojista/listar');
+    },
+
+    // listagem de pedidos 
+    async ordersList (req,res,next) {
+      
+    
+    res.render('pedidos_lojista');  
+  }
+}  
