ce2a4f1b7f33947896bdb4117ed5fc6dddbe8cd1
Marco Campos <marcoaadc@gmail.com>
2021-02-27 18:28:23 -0300

    adicionando session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var pedidoRouter = require('./routes/pedido');
@@ -15,6 +16,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: "projeto-food-ecommerce",
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
