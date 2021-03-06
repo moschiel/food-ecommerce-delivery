eb422e490941f317d2e552d8174902d5b7665edc
moschiel <moschiel@hotmail.com>
2021-03-06 20:10:45 -0300

    migration dos dados do lojista corrigido

--- database/migrations/20210221174408-create_shopkeeper_table.js
+++ database/migrations/20210221174408-create_shopkeeper_table.js
@@ -3,8 +3,14 @@
 module.exports = {
   up: async (queryInterface, DataType) => {
     queryInterface.createTable('shopkeeper', {
+      id: {
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       username: DataType.STRING,
       password: DataType.STRING,
+      email: DataType.STRING,
       createdAt: {
         type: DataType.DATE
       },
--- models/Shopkeeper.js
+++ models/Shopkeeper.js
@@ -1,21 +1,24 @@
 module.exports = (sequelize, DataType) => {
 	const Shopkeeper = sequelize.define(
-        'Shopkeeper', 
-        {
-          username: {
-            type: DataType.STRING,
-            primaryKey: true,
-          },             
-            password: DataType.STRING(255),
-            email: DataType.STRING,
-            createdAt: {
-              type: DataType.DATE
-            },
-            updatedAt: {
-              type: DataType.DATE
-            }
-        },{
-            tableName: 'shopkeeper',
-            });
-            return Shopkeeper;
-        };
+    'Shopkeeper', 
+    {
+      id: {
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+        username: DataType.STRING,            
+        password: DataType.STRING,
+        email: DataType.STRING,
+        createdAt: {
+          type: DataType.DATE
+        },
+        updatedAt: {
+          type: DataType.DATE
+        }
+    },
+    {
+        tableName: 'shopkeeper',
+    });
+    return Shopkeeper;
+};
