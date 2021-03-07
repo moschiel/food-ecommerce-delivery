9ad2ef56a56d4b976c328a54bdabc4c33c3ad7a2
moschiel <moschiel@hotmail.com>
2021-03-06 22:47:20 -0300

    se usuario tentar entrar na pagina de pedido sem estar logado, é redirecionado para paragina de login

--- controllers/PedidoController.js
+++ controllers/PedidoController.js
@@ -1,7 +1,17 @@
 
 module.exports = {
   async solicitar(req, res, next) {
-    res.render('pedido_solicitar');
+    //se existir usuario logado, vai pra pagina de pedido
+    if(req.session.user)
+    {
+      res.render('pedido_solicitar', { user: req.session.user } );
+    }
+    //senão, força o usuário a logar
+    else 
+    {
+      res.redirect('/usuario/autenticar')
+    }
+    
   }
 }
 
